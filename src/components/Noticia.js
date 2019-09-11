9bb010f99a50eba5656613eeddc999e64b789b04
aledia <alediaortega@gmail.com>
2019-09-11 13:05:47 +0200

    agregando form

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
 
-const Noticia = () => {
+const Noticia = ({noticia}) => {
+
+    // extraer datos
+    const { urlToImage, url, title, description, source } = noticia;
+    // cargar img si la noticia contiene una..
+        const imagen = (urlToImage) ? 
+            <div className="card-image">
+                <img src={urlToImage} alt={title}/>
+                <span className="card-title"> {source.name} </span>
+            </div>
+        : null
+
+
+
+
     return(
-    <h1>Desde noticias</h1>
+        <div className="col s12 m6 l4">
+            <div className="card">
+                {imagen}
+
+                <div>
+                    <h3 className="titulo">{title}</h3>
+                    <p className="  v">{description}</p>
+                </div>
+                <div className="card-action">
+                    <a href={url} target="_blank" rel="noopener noreferrer" className="waves-effect waves-ligth btn"> Ver NOticia completa</a>
+                </div>
+            </div>
+        </div>
     )
 
 };
